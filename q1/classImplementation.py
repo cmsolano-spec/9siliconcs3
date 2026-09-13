e96e95f3cd1e08154f5a958b0ecd18bfc48393ea
cmsolano-spec <cmsolano@brc.pshs.edu.ph>
2026-09-13 18:50:41 +0800

    Update classImplementation.py

--- q1/classImplementation.py
+++ q1/classImplementation.py
@@ -6,16 +6,16 @@ class Plants:
     self._height = height
     self._waterstatus = waterstatus
     
-  def waterplant(self):
+  def _waterplant(self):
     self.__waterstatus = True
     
-  def grow(self, amount):
+  def _grow(self, amount):
     return self._height + amount
 
   def displayinfo(self):
     print(f"The name of the plant is {self.name}")
     
-  def needswater(self):
+  def _needswater(self):
     if self._waterstatus is False:
       print("The plant needs to be watered")
     
@@ -27,7 +27,7 @@ print(f"Basil's Information: \n Name: {basil.name} \n Color: {basil.color} \n He
 print(f"Cactus's Information: \n Name: {cactus.name} \n Color: {cactus.color} \n Height: {cactus._height} inches \n Water Status: {cactus._waterstatus} \n")
 
 print("Increasing Basil's height by 2 inches \n")
-basil._height = basil.grow(2)
+basil._height = basil._grow(2)
 
 print("--- AFTER ---")
 print(f"Basil's Information: \n Name: {basil.name} \n Color: {basil.color} \n Height: {basil._height} inches \n Water Status: {basil._waterstatus} \n")
