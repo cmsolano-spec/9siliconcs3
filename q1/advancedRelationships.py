a2c18aaca6810bfe2299310eb43ec68f1194694e
cmsolano-spec <cmsolano@brc.pshs.edu.ph>
2026-09-25 00:22:56 +0800

    Enhance Sunflower class with new methods
    Added methods for watering, growing, and displaying info about the Sunflower class.

--- q1/advancedRelationships.py
+++ q1/advancedRelationships.py
@@ -26,4 +26,20 @@ class Sunflower(Plants):
 
   def __init__(self, name, color, height, waterstartus, number):
     super().__init__(value)
-    self.number = number
+    self.number = number 
+    
+  def _waterplant(self):
+    self._waterstatus = True
+    
+  def _grow(self, amount):
+    return self._height + amount
+    
+  def displayinfo(self):
+    print(f"The name of the plant is {self.name}")
+    print(f"The color of the plant is {self.color}")
+    print(f"The height of the plant is {self._height}")
+    print(f"The water status of the plant is {self._waterstatus}")
+    
+  def _needswater(self):
+    if self._waterstatus is False:
+      print("The plant needs to be watered")
